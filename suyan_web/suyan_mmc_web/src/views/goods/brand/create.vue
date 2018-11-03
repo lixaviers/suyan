a659ae1e704679fa8e835167762edf51e53952dd
dongxian.li <dongxian.li@chinaredstar.com>
2018-11-03 11:15:49 +0800

    log

---
+++ suyan_web/suyan_mmc_web/src/views/goods/brand/create.vue
@@ -0,0 +1,101 @@
+<template>
+  <Form style="margin-top: 10px; padding-right: 20px;" ref="createBrandForm" :rules="ruleValidate" :model="formBrand"
+        :label-width="120">
+    <FormItem label="品牌名称" prop="name">
+      <Input v-model="formBrand.name" placeholder="品牌名称" style="width: 320px"/>
+    </FormItem>
+    <FormItem label="品牌英文名称" prop="nameEn">
+      <Input v-model="formBrand.nameEn" placeholder="品牌英文名称" style="width: 320px"/>
+    </FormItem>
+    <FormItem label="所属类目">
+      <Cascader :data="data" v-model="formBrand.categoryId" style="width: 320px"></Cascader>
+    </FormItem>
+    <FormItem label="品牌描述" prop="description">
+      <Input v-model="formBrand.description" type="textarea" :autosize="{minRows: 3,maxRows: 5}" placeholder="品牌描述"/>
+    </FormItem>
+    <FormItem>
+      <Button :loading="loading" type="primary" @click="handleSubmit('createBrandForm', 1)">提 交</Button>
+      <Button @click="$router.push({name: 'goodsBrandIndex'});" style="margin-left: 15px;">返回品牌列表</Button>
+    </FormItem>
+  </Form>
+</template>
+<script>
+  import {treeDataTranslate} from '@/utils';
+  export default {
+    data () {
+      return {
+        data: [],
+        loading: false,
+        formBrand: {
+          id: null,
+          categoryId: [],
+          name: '',
+          nameEn: '',
+          description: '',
+        },
+        ruleValidate: {
+          name: [
+            {required: true, message: '品牌名称不能为空', trigger: 'blur'}
+          ],
+        },
+      }
+    },
+    created () {
+      let id = this.$route.params.id;
+      this.$http({
+        url: this.$http.adornUrl('category/queryCategoryCascader', 2),
+        method: 'get',
+        data: this.$http.adornData()
+      }).then(({data}) => {
+        if (data && data.code === 200 && data.dataMap) {
+          var data = data.dataMap;
+          this.data = treeDataTranslate(data);
+          console.log(this.data)
+        }
+      });
+
+
+      if (id) {
+        this.$http({
+          url: this.$http.adornUrl('brand/getBrand/' + id, 2),
+          method: 'get',
+          data: this.$http.adornData()
+        }).then(({data}) => {
+          if (data && data.code === 200 && data.dataMap) {
+            this.formBrand.id = id;
+            this.formBrand = data.dataMap;
+            console.log(this.formBrand)
+          }
+        });
+      }
+    },
+    methods: {
+      handleSubmit (name) {
+        let t = this;
+        this.$refs[name].validate((valid) => {
+          if (valid) {
+            this.loading = true;
+            this.$http({
+              url: this.$http.adornUrl(`brand/${!this.formBrand.id ? 'createBrand' : 'updateBrand'}`, 2),
+              method: 'post',
+              data: this.$http.adornData(this.formBrand)
+            }).then(({data}) => {
+              if (data && data.code === 200) {
+                this.$Message.success({
+                  content: '操作成功',
+                  duration: 2,
+                  onClose: function () {
+                    t.$router.push({name: 'goodsBrandIndex'});
+                  }
+                });
+              } else {
+                this.loading = false;
+              }
+            })
+
+          }
+        })
+      }
+    }
+  }
+</script>
